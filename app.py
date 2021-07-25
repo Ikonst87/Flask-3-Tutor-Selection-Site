a9edf7b3d22b82e1c7984ee2e55263ab76bd001e
ikonst87 <ikonstantinovskyi@bostonsd.ru>
2021-07-25 21:03:09 +0300

    Add routes

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def render_main():
+    return "Здесь будет главная страница"
+
+
+@app.route("/all")
+def render_all_tutors():
+    return "Здесь будет страница со всеми преподавателями"
+
+
+@app.route("/goals/<goal>/")
+def render_goals(goal):
+    return "Здесь будут <goal>"
+
+
+@app.route("/profile/<tutor_id>/")
+def render_tutor_profile(tutor_id):
+    return "Здесь будет профиль репетитора"
+
+
+@app.route("/request/")
+def render_request_form():
+    return "Здесь будет форма подбора репетиторов"
+
+
+@app.route("/request_done")
+def render_success_request_form():
+    return "Здесь будет форма при успешном заполнение формы"
+
+
+@app.route("/booking/<tutor_id>/<day_of_week>/<time>")
+def render_booking_form():
+    return "Здесь будет форма для бронирования занятий"
+
+
+@app.route("/booking_done")
+def render_success_booking_form():
+    return "Здесь страница успешного бронирования занятия"
